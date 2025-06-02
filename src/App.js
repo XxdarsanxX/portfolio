22c1e19a0ea2de119fbc6b5b0cd99bf9329a4324
XxdarsanxX <darsan.muhamad@gmail.com>
2025-06-02 19:39:42 +0100

    change of picture

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import './App.css';
-import mePic from './assets/me.png';
+import mePic from './assets/me1.png';
 
 function App() {
   return (
